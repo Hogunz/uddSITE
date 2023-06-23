0bfbc28eba5dd9a354ea61de2ff5cadb776d9823
anonymous <cjdc1414@gmail.com>
2023-06-23 23:16:03 +0800

    updated

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -9,13 +9,24 @@
         <div class="row row-cols-1 row-cols-md-3">
 
             @foreach ($events->shuffle()->take(6) as $event)
-                <div class="card-group col mb-4">
+                {{-- <div class="card-group col mb-4">
                     <div class="card ">
                         <img src="{{ asset('storage/' . $event->image) }}" class="img-fluid">
                         <div class="card-body">
                             <h5 class="card-title">
                                 <a href="{{ route('event.show', $event->id) }}"> <span
                                         class="font-weight-bold text-sm-xs text-primary ">{{ $event->name }}</span></a>
+                            </h5>
+                            <p>{{ $event->user->name }} <span>{{ $event->created_at }}</span></p>
+                        </div>
+                    </div>
+                </div> --}}
+                <div class="card-columns">
+                    <div class="card mb-4">
+                        <img src="{{ asset('storage/' . $event->image) }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="{{ route('event.show', $event->id) }}"><span
+                                        class="font-weight-bold text-sm-xs text-primary">{{ $event->name }}</span></a>
                             </h5>
                             <p>{{ $event->user->name }} <span>{{ $event->created_at }}</span></p>
                         </div>
